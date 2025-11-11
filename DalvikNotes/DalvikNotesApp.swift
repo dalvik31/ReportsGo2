11764a48e34bf4be075ef614044482c22256827c
ISRAEL JUAREZ <isra.juarez1821@gmail.com>
2025-11-11 12:46:21 -0600

    Initial Commit

---
+++ DalvikNotes/DalvikNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DalvikNotesApp.swift
+//  DalvikNotes
+//
+//  Created by Emmanuel Pacheco on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DalvikNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DalvikNotesTests/DalvikNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DalvikNotesTests.swift
+//  DalvikNotesTests
+//
+//  Created by Emmanuel Pacheco on 11/11/25.
+//
+
+import Testing
+@testable import DalvikNotes
+
+struct DalvikNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
